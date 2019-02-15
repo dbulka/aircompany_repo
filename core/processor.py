cf42e216c703d3b7eef89d562875f0021259f54b
litsvin <kratovich.a@yandex.ru>
2019-02-16 00:10:59 +0300

    paths parametrization

--- core/processor.py
+++ core/processor.py
@@ -1,11 +1,12 @@
 from core.controllers.planes.planes_controller import PlanesController
 from core.models.planes.plane import Plane
+from utils.context import Context, Parameter
 from utils.reader import read_txt
 from utils.writer import write_2_json
 
 
 def planes_2_json():
-    planes = read_txt('/Users/akratovich/projects/python/aircompany_repo/resources/planes_sample.txt')
+    planes = read_txt('{0}/planes_sample.txt'.format(Context.get(Parameter.BASE_RESOURCE_PATH)))
     planes_list = []
     for line in planes:
         if line:
@@ -23,7 +24,7 @@ def parse_line_from_planes_source(line):
 
 
 def planes_2_db():
-    planes = read_txt('/Users/akratovich/projects/python/aircompany_repo/resources/ACFTREF.txt')
+    planes = read_txt('{0}/ACFTREF.txt'.format(Context.get(Parameter.BASE_RESOURCE_PATH)))
     plane_ctrl = PlanesController()
     for line in planes:
         if line:
--- utils/context.py
+++ utils/context.py
@@ -1,15 +1,15 @@
-import os
 import configparser
+import os
 from enum import Enum
 
-# CONFIG_FILE_PATH = os.path.join(os.getcwd(), 'aircompany.ini')
-CONFIG_FILE_PATH = r'/Users/akratovich/projects/python/aircompany_repo/tests/aircompany.ini'
+CONFIG_FILE_PATH = os.path.join(os.getcwd(), 'aircompany.ini')
 
 
 class Context:
     """
     Contains parser methods of config from a different number of sections of CONFIG_FILE_PATH
     """
+
     @classmethod
     def get(cls, parameter):
         """
@@ -27,3 +27,4 @@ class Parameter(Enum):
     DB_PASSWORD = 'dbpassword'
     DB_HOST = 'dbhost'
     DB_NAME = 'dbname'
+    BASE_RESOURCE_PATH = 'base_resorce_path'
