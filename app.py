de5eec6d7c1e69100f57b0ae5b650d48c54b455c
dbulka <dzm.bulka@gmail.com>
2019-02-23 20:08:36 +0300

    flask_v1.2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from core.controllers.flight.flights_controller import FlightsController
+from core.controllers.flights.flights_controller import FlightsController
 
 
 flights = FlightsController()
@@ -10,37 +10,30 @@ def get_all_flights():
     read_all_flights = flights.read()
     return str(read_flight)
 
-@app.route('api/flights/<id int>')
+@app.route('api/flights/<id int>', method = 'GET')
 def get_flight_by_id(id):
     read_the_flight = flights.read_id(id)
     return print(read_the_flight)
 
-@app.route('api/flights/)
+@app.route('api/flights/',  method = 'DELETE')
 def delete_flights():
     flights.delete_all()
     return 'all flights were deleted'
 
-@app.route('api/flights/<id int>')
+@app.route('api/flights/<id int>', method = 'DELETE')
 def delete_the_flight(id):
     flights.delete_by_id(id)
     return 'flights with {0} was deleted'.format(id)
 
-@app.route('api/flights/')
-    def create(
-        flights.create(id_plane, _from, _to, date_departure, date_arrival)
+@app.route('api/flights/', method = 'create')
+def create(id_plane, _from, _to, date_departure, date_arrival):
+    flights.create(id_plane, _from, _to, date_departure, date_arrival)
+    return 'flight was created'
+
+@app.route('api/flights/<id int>', method = 'EDIT')
+def edit(id):
+    flights.update(_to, id_plane)
+    return 'flight was edited'
 
 
-
-# @app.route('/api/flights/create/')
-# def create():
-#     flight.create(3, "'MINSK'", "'NEW YORK'", "'20-02-12 19:00'", "'21-02-15 19:00'")
-#     return "created"
-#
-# @app.route('/api/flights/delete/')
-# def delete():
-#     flight.delete(3)
-#     return "deleted"
-#
-# if __name__ == "__main__":
-#     app.run()
 
